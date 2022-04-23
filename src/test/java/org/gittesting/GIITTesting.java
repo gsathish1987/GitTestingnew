73dc663774e14eb3b5592ee8997ddc6f9a2c3160
gsathish1987 <gsathish.k1987@gmail.com>
2022-04-23 21:37:00 +0530

    new code

---
+++ src/test/java/org/gittesting/GIITTesting.java
@@ -0,0 +1,10 @@
+package org.gittesting;
+
+public class GIITTesting {
+	public static void main(String[] args) {
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
+	}
+
+}
