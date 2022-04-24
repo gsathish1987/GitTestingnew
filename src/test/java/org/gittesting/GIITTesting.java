2e894d1271d6505a8ca0e327d56b804680268709
gsathish1987 <gsathish.k1987@gmail.com>
2022-04-24 08:43:15 +0530

    Subash's code

--- src/test/java/org/gittesting/GIITTesting.java
+++ src/test/java/org/gittesting/GIITTesting.java
@@ -2,9 +2,9 @@ package org.gittesting;
 
 public class GIITTesting {
 	public static void main(String[] args) {
-		System.out.println("Test1");
-		System.out.println("Test2");
-		System.out.println("Test3");
+		System.out.println("Test7");
+		System.out.println("Test8");
+		System.out.println("Test9");
 	}
 
 }
